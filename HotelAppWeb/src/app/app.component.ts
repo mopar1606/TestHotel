5a2f7e9457ef7245b3b28477b4222da8c419470e
mopar1606 <mopar1606@gmail.com>
2024-04-24 00:39:42 -0500

    Avance 12.39

--- HotelAppWeb/src/app/app.component.ts
+++ HotelAppWeb/src/app/app.component.ts
@@ -5,7 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'HotelAppWeb';
